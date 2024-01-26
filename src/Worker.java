76a9d8e82689b39f00be194b6777d45d09aaa326
Alex <alexbatul@mail.ru>
2024-01-26 16:53:50 +0600

    second commit

---
+++ src/Worker.java
@@ -0,0 +1,31 @@
+public class Worker {
+    @FunctionalInterface
+    public interface OnTaskDoneListener {
+        void onDone(String result);
+    }
+
+    @FunctionalInterface
+    public interface OnTaskErrorListener {
+        void onError(String result);
+    }
+
+    private OnTaskDoneListener callback;
+    private OnTaskErrorListener errorCallback;
+
+
+    public void start() {
+        for (int i = 0; i < 100; i++) {
+            callback.onDone("Task " + i + " is done");
+            if (i == 33) {
+                errorCallback.onError("Task " + i + "is error");
+
+            }
+
+        }
+    }
+
+    public Worker(OnTaskDoneListener callback, OnTaskErrorListener errorCallback) {
+        this.callback = callback;
+        this.errorCallback = errorCallback;
+    }
+}
